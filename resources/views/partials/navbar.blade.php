11fc9e706917c54a7c13b2fa6aa95cfcda9be0af
wahyu070405 <wahyusuhendra0505@gmail.com>
2025-10-20 12:49:55 +0700

    coba

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | Web Laundry</title>
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+</head>
+<body>
+    <header>
+        <nav>
+            
+        </nav>
+    </header>
+</body>
+</html>
